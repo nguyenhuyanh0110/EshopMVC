76815c0ef7d24f3f1a652cb42f725e6d7f957f4b
huyanh <nguyenhuyanh2222@gmail.com>
2021-07-08 10:32:38 +0700

    create product and validation data

--- Model/Entity/CATEGORY.cs
+++ Model/Entity/CATEGORY.cs
@@ -1,7 +1,8 @@
-namespace Model.Entity
+﻿namespace Model.Entity
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -9,9 +10,12 @@ namespace Model.Entity
     [Table("CATEGORY")]
     public partial class CATEGORY
     {
+        [DisplayName("Mã số danh mục")]
         public int CATEGORYID { get; set; }
 
         [StringLength(250)]
+        [DisplayName("Tên danh mục")]
+        [Required(ErrorMessage = ("Vui lòng điền vào danh mục"))]
         public string CATEGORYNAME { get; set; }
     }
 }
