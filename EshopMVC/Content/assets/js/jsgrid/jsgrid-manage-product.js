5b8b0ba36518dd48f5d87ff8cbfbab36955550e2
huyanh <nguyenhuyanh2222@gmail.com>
2021-07-12 23:06:59 +0700

    change UI template and list category with jsGrid

---
+++ EshopMVC/Content/assets/js/jsgrid/jsgrid-manage-product.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict";
+    $("#basicScenario").jsGrid({
+        width: "100%",
+        filtering: true,
+        editing: true,
+        inserting: true,
+        sorting: true,
+        paging: true,
+        autoload: true,
+        pageIndex: true,
+        pActionSize: 15,
+        pActionButtonCount: 5,
+        deleteConfirm: "Do you really want to delete the client?",
+        controller: {
+            loadData: function () {
+                return $.ajax({
+                    type: "GET",
+                    url: "/Category/GetCategory",
+                    dataType: "json",
+                });
+            }
+        },
+        fields: [
+            { name: "CATEGORYID", type: "text", title: "Category Id" ,width: 100 },
+            { name: "CATEGORYNAME", type: "text", title: "Category Name" ,width: 50 },
+            { type: "control" }
+        ]
+    });
+})(jQuery);
